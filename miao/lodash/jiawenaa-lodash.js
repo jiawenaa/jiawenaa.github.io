07ae02e4f6e981c967e35bed2f6921dcee1830f6
jiawen <1003368813@qq.com>
2020-12-08 17:13:36 +0800

    1

---
+++ miao/lodash/jiawenaa-lodash.js
@@ -0,0 +1,21 @@
+var  jiawenaa = function(){
+    function chunk(array,size){   
+        let b = []
+        for(var i = 0 ; i < array.length ; i){
+            let c = size
+            let a = []
+            while(c){
+                if(array[i]){
+                    a.push(array[i])
+                    i++
+                    c--
+                }else{
+                    break
+                }
+                
+            }
+            b.push(a)
+        }
+        return b,
+    }
+}
