1158601d2af4d016632d73e21d49e83b00183d3a
jiawen <1003368813@qq.com>
2020-12-08 21:12:45 +0800

    3

--- miao/lodash/jiawenaa-lodash.js
+++ miao/lodash/jiawenaa-lodash.js
@@ -34,7 +34,7 @@ var  jiawenaa = function () {
         for(var i = 0; i < a ; i++){
             let b = arguments[i]
             let c = b.length
-            if(Arry.isArry(arguments[i])){
+            if(Array.isArray(arguments[i])){
                 for(var j = 0; j < c ; j++){
                     result.push(b[j])
                 }
